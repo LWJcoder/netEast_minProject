53881a2f7ea626b0cf2834845024a8a4a62a7e4e
LWJgithub <15818464582@163.com>
2017-11-09 00:24:58 +0800

    更新
    修复问题

--- js/pageAction.js
+++ js/pageAction.js
@@ -102,104 +102,10 @@
 		
 	}
 
-	function closeModal (){
-		modal.hide();
-		//去除灰色样式
-		var pa = document.getElementsByClassName("pageArea");
-		pa[0].className = " pageArea ";
-	}
 
-	//登录
-	function login(){						
-			var data = {};
-				
-			data.userName = document.getElementById('username').value;
-			data.password =  document.getElementById('password').value;
-			console.log("登录"+data.userName)
-			var ajax = new Ajax();
-			ajax.get("http://study.163.com/webDev/login.htm",data, function(data){
-				if (data == 1){
-					//登录成功
-					var tem = ' <div class="m-modal">\
-								  <div class="modal_align"></div>\
-								  <div class="modal_wrap">\
-								    <div class="modal_head">恭喜</div>\
-								    <div class="modal_body">登录成功</div>\
-								    <div class="modal_foot">\
-								      <a  href="#">确认</a>\
-								      <a  href="#">取消</a>\
-								    </div>\
-								  </div>\
-								</div>';
-					var modal = new Modal({
-						  // 1. 内容配置
-						  content: tem, //可传入节点和字符串
-						  // 2. 动画设置
-						  animation: {
-						    enter: 'bounceIn',
-						    leave: 'bounceOut'
-						  }
-						});	
-					modal.show();		
-
-				}else{
-						//登录失败
-					var temp = ' <div class="m-modal">\
-								  <div class="modal_align"></div>\
-								  <div class="modal_wrap">\
-								    <div class="modal_head">失败</div>\
-								    <div class="modal_body">登录失败，请重新输入</div>\
-								    <div class="modal_foot">\
-								      <a href="#">确认</a>\
-								      <a  href="#">取消</a>\
-								    </div>\
-								  </div>\
-								</div>';
-					var modal1 = new Modal({
-						  // 1. 内容配置
-						  content: temp, //可传入节点和字符串
-						  // 2. 动画设置
-						  animation: {
-						    enter: 'bounceIn',
-						    leave: 'bounceOut'
-						  }
-						});	
-					modal1.show();	
-				}
-			})		
-								
-		}
 	
 	
 
- var content1 = '<div class="modal_head">\
-      <span><i class="xP m-close" onclick="closeModal()"></i></span>\
-      <h3>登录网易云课堂</h3></div>\
-      <div class="modal_body">\
-      <form id="loginForm" action="http://study.163.com/webDev/login.htm" method="get" autocomplete="false">\
-        <div class="inputArea">\
-          <input type="text" class="m-logName" id="username" name="userName" placeholder="账户" />\
-          <br />\
-          <input type="password" class="psw" id="password" minlength="6" placeholder="密码" />\
-          <br />\
-          <button type="button"  class="loginBtn" onclick="login()">登录</button>\
-        </div>\
-      </form>\
-      </div>';
-	var loginModal = new Modal({
-	  // 1. 内容配置
-	  content: content1, //可传入节点和字符串
-	  // 2. 动画设置
-	  animation: {
-	    enter: 'bounceIn',
-	    leave: 'bounceOut'
-	  }
-	});
-
-	var watch = document.getElementById("m-subscri");
-	watch.onclick = function(){
-		loginModal.show();
-	}
 
 
 	function noRemind(){
